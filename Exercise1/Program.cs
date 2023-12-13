8a80ae73eb7135ec99e7c23c2d68c31c334dc880
Anastasiy <Leonardo0907@mail.ru>
2023-12-14 02:32:22 +0300

    Задача 1

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива,
+//значения которых лежат в отрезке [20,90].
+
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());;
+// int[] array = new int[n];
+// for(int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(1, 101);
+// Console.WriteLine(string.Join(", ", array));
+
+// int result = 0;
+// for(int i = 0; i < array.Length; i++)
+// if(array[i] <= 90 && array[i] >= 20)
+// result++;
+// Console.WriteLine(result);
+
